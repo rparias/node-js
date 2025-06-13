b9c0d3bd87e88d6df65819e900d0a048cca76e9e
Ronald Arias <ariasron@hotmail.com>
2025-06-13 16:34:59 -0500

    adds post to create pokemon

--- api-express/index.js
+++ api-express/index.js
@@ -7,6 +7,21 @@ app.get('/', (req, res) => {
   res.end('Hello, World!');
 });
 
+app.post('/pokemon', (req, res) => {
+  let body = '';
+  
+  req.on('data', chunk => {
+    body += chunk.toString(); // Convert Buffer to string
+  });
+
+  req.on('end', () => {
+    console.log('Received data:', body);
+    const jsonData = JSON.parse(body);
+    jsonData.timestamp = new Date().toISOString();
+    res.status(201).json(jsonData);
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
