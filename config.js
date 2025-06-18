cf59992390ffda570fb1a987a8e8b9f3f84c5396
Ronald Arias <ariasron@hotmail.com>
2025-06-18 15:31:44 -0500

    adds jwt and cookieparser to handle login and protected route

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 export const {
   PORT = 3000,
-  SALT_ROUNDS = 10
+  SALT_ROUNDS = 10,
+  SECRET_JWT_KEY = 'this_is_an_absolute_amazing_beautiful_secret_key_and_you_should_not_use_it_in_production'
 } = process.env
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from 'express'
-import { PORT } from './config.js'
+import jwt from 'jsonwebtoken'
+import cookieParser from 'cookie-parser'
+import { PORT, SECRET_JWT_KEY } from './config.js'
 import { UserRepository } from './user-repository.js'
 
 const app = express()
@@ -7,6 +9,7 @@ const app = express()
 app.set('view engine', 'ejs') // Set EJS as the view engine
 
 app.use(express.json()) // Middleware to parse JSON bodies
+app.use(cookieParser()) // Middleware to parse cookies
 
 app.disable('x-powered-by') // Disable 'X-Powered-By' header for security
 
@@ -20,7 +23,19 @@ app.post('/login', async (req, res) => {
 
   try {
     const user = await UserRepository.login({ username, password })
-    res.json({ user })
+    const token = jwt.sign(
+      { id: user._id, username: user.username },
+      SECRET_JWT_KEY,
+      { expiresIn: '1h' }
+    )
+    res
+      .cookie('access_token', token, {
+        httpOnly: true, // Cookie is not accessible via JavaScript, only available on the server side
+        secure: process.env.NODE_ENV === 'production', // Use secure cookies in production
+        sameSite: 'Strict', // Prevent CSRF attacks. Only availabe from same domain
+        maxAge: 3600000 // 1 hour
+      })
+      .json({ user })
   } catch (error) {
     res.status(401).json({ error: error.message })
   }
@@ -40,7 +55,18 @@ app.post('/register', async (req, res) => {
 
 app.post('/logout', (req, res) => {})
 
-app.post('/protected', (req, res) => {})
+app.get('/protected', (req, res) => {
+  const token = req.cookies.access_token
+
+  if (!token) return res.status(401).json({ error: 'Unauthorized' })
+
+  try {
+    const data = jwt.verify(token, SECRET_JWT_KEY)
+    res.render('protected', data) // { _id, username }
+  } catch (error) {
+    res.status(401).json({ error: 'Unauthorized' })
+  }
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`)
